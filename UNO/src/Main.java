5de13b46ca4fb8855de99a97589cddbb44cd57f7
colinm1215 <colinm1215@gmail.com>
2019-09-03 10:44:45 -0400

    Added basis of GUI and MAIN

--- UNO/src/Main.java
+++ UNO/src/Main.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String args[]) {
+        GUI frame = new GUI();
+        frame.setTitle("UNO");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(1200, 800);
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
 }
+
